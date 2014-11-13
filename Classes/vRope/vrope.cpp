352c845c18a175538babeba96bfa72deea4b2d88
Mohammed Header <mohammed.header@espace.com.eg>
2014-11-13 17:31:54 +0200

    Add vRope ( Modified )

---
+++ Classes/vRope/vrope.cpp
@@ -0,0 +1,209 @@
+/*
+ 
+ MIT License.
+
+ Copyright (c) 2012 Creeng Ltd.
+ Copyright (c) 2012 Flightless Ltd.  
+ Copyright (c) 2010 Clever Hamster Games.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+*/
+
+//
+//  vrope.cpp
+//
+//  Created by patrick on 14/10/2010.
+//  Modified by Flightless www.flightless.co.nz 20/4/2012
+//  Ported to cocos2d-x by Creeng www.creeng.com 12/7/2012
+//
+
+#include "vrope.h"
+
+using namespace cocos2d;
+
+// Flightless, init rope using a joint between two bodies
+VRope::VRope(b2RopeJoint* joint, CCSpriteBatchNode* ropeBatchNode)
+    : joint(joint), spriteSheet(ropeBatchNode)
+{
+    Vec2 pointA = Vec2(jointAB->GetAnchorA().x*PTM_RATIO,jointAB->GetAnchorA().y*PTM_RATIO);
+    Vec2 pointB = Vec2(jointAB->GetAnchorB().x*PTM_RATIO,jointAB->GetAnchorB().y*PTM_RATIO);
+    createRope(pointA, pointB,joint->GetMaxLength()*PTM_RATIO);
+}
+
+VRope::~VRope()
+{
+    for(int i=0; i<numPoints; ++i)
+    {
+        delete vPoints[i];
+        if (i!=numPoints-1) {
+            delete vSticks[i];
+        }
+    }
+}
+
+void VRope::update(float dt)
+{
+    Vec2 pointA = Vec2(joint->GetAnchorA().x*PTM_RATIO,joint->GetAnchorA().y*PTM_RATIO);
+    Vec2 pointB = Vec2(joint->GetAnchorB().x*PTM_RATIO,joint->GetAnchorB().y*PTM_RATIO);
+    updateWithPoints(pointA, pointB, dt);
+}
+
+void VRope::reset()
+{
+    Vec2 pointA = Vec2(joint->GetAnchorA().x*PTM_RATIO,joint->GetAnchorA().y*PTM_RATIO);
+    Vec2 pointB = Vec2(joint->GetAnchorB().x*PTM_RATIO,joint->GetAnchorB().y*PTM_RATIO);
+    resetWithPoints(pointA, pointB);
+}
+    
+void VRope::createRope(const CCPoint& pointA, const CCPoint& pointB, float distance)
+{
+//    float distance = ccpDistance(pointA,pointB);
+    int segmentFactor = 20; // 16; //12; //increase value to have less segments per rope, decrease to have more segments
+    numPoints = (int) distance/segmentFactor;
+
+    CCPoint diffVector = ccpSub(pointB,pointA);
+    float multiplier = distance / (numPoints-1);
+    antiSagHack = 0.1f; //HACK: scale down rope points to cheat sag. set to 0 to disable, max suggested value 0.1
+    for(int i=0;i<numPoints;i++) {
+            CCPoint tmpVector = ccpAdd(pointA, ccpMult(ccpNormalize(diffVector), multiplier*i*(1-antiSagHack)));		
+    VPoint *tmpPoint = new VPoint();
+    tmpPoint->setPos(tmpVector.x, tmpVector.y);
+    vPoints.push_back(tmpPoint);
+    }
+    for(int i=0;i<numPoints-1;i++) {
+    VStick* tmpStick = new VStick(vPoints[i], vPoints[i+1]);
+    vSticks.push_back(tmpStick);
+    }
+
+    if(spriteSheet) {
+	for(int i=0;i<numPoints-1;i++) {
+            VPoint* point1 = vSticks[i]->getPointA();
+            VPoint* point2 = vSticks[i]->getPointB();
+            CCPoint stickVector = ccpSub(ccp(point1->x,point1->y),ccp(point2->x,point2->y));
+            float stickAngle = ccpToAngle(stickVector);
+
+            float f = spriteSheet->getTextureAtlas()->getTexture()->getPixelsHigh() / CC_CONTENT_SCALE_FACTOR();
+            CCRect r = CCRectMake(0, 0, multiplier, f);
+            CCSprite* tmpSprite = CCSprite::createWithTexture(spriteSheet->getTexture(), r);
+            
+            Texture2D::TexParams params = {GL_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT};
+            tmpSprite->getTexture()->setTexParameters(&params);
+            tmpSprite->setPosition(ccpMidpoint(ccp(point1->x, point1->y), ccp(point2->x, point2->y)));
+            tmpSprite->setRotation(-1 * CC_RADIANS_TO_DEGREES(stickAngle));
+            spriteSheet->addChild(tmpSprite);
+            ropeSprites.push_back(tmpSprite);
+        }
+    }
+}
+
+void VRope::resetWithPoints(const CCPoint &pointA, const CCPoint& pointB)
+{
+	float distance = ccpDistance(pointA, pointB);
+	CCPoint diffVector = ccpSub(pointB, pointA);
+	float multiplier = distance / (numPoints - 1);
+	for(int i=0;i<numPoints;i++) {
+		CCPoint tmpVector = ccpAdd(pointA, ccpMult(ccpNormalize(diffVector), multiplier*i*(1-antiSagHack)));
+        VPoint *tmpPoint = vPoints[i];
+        tmpPoint->setPos(tmpVector.x, tmpVector.y);
+	}
+}
+
+void VRope::updateWithPoints(const CCPoint& pointA, const CCPoint& pointB, float dt)
+{
+    //manually set position for first and last point of rope
+    vPoints[0]->setPos(pointA.x, pointA.y);
+    vPoints[numPoints -1]->setPos(pointB.x, pointB.y);
+	
+	//update points, apply gravity
+	for(int i=1;i<numPoints-1;i++) {
+        vPoints[i]->applyGravity(dt);
+        vPoints[i]->update();
+	}
+	
+	//contract sticks
+	int iterations = 4;
+	for(int j=0;j<iterations;j++) {
+		for(int i=0;i<numPoints-1;i++) {
+            vSticks[i]->contract();
+		}
+	}
+}
+
+void VRope::updateWithPoints(const CCPoint& pointA, const CCPoint& pointB, float gxdt, float gydt)
+{
+	//manually set position for first and last point of rope
+    vPoints[0]->setPos(pointA.x, pointA.y);
+    vPoints[numPoints-1]->setPos(pointB.x, pointB.y);
+	
+	//update points, apply pre-integrated gravity
+	for(int i=1;i<numPoints-1;i++) {
+        vPoints[i]->applyGravityxdt(gxdt, gydt);
+        vPoints[i]->update();
+	}
+	
+	//contract sticks
+	int iterations = 4;
+	for(int j=0;j<iterations;j++) {
+		for(int i=0;i<numPoints-1;i++) {
+            vSticks[i]->contract();
+		}
+	}
+}
+
+void VRope::debugDraw()
+{
+    //Depending on scenario, you might need to have different Disable/Enable of Client States
+	//glDisableClientState(GL_TEXTURE_2D);
+	//glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	//glDisableClientState(GL_COLOR_ARRAY);
+	//set color and line width for ccDrawLine
+
+    ccDrawColor4F(0.0f, 0.0f, 1.0f, 1.0f);
+    glLineWidth(5.0f);
+
+	for(int i=0;i<numPoints-1;i++) {
+		//"debug" draw
+		VPoint *pointA = vSticks[i]->getPointA();
+		VPoint *pointB = vSticks[i]->getPointB();
+		ccDrawPoint(ccp(pointA->x,pointA->y));
+		ccDrawPoint(ccp(pointB->x,pointB->y));
+		//ccDrawLine(ccp(pointA.x,pointA.y),ccp(pointB.x,pointB.y));
+	}
+
+    //restore to white and default thickness
+	ccDrawColor4F(1.0f,1.0f,1.0f,1.0f);
+    glLineWidth(1);
+	//glEnableClientState(GL_TEXTURE_2D);
+	//glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	//glEnableClientState(GL_COLOR_ARRAY);
+}
+
+void VRope::updateSprites()
+{
+	if(spriteSheet) {
+		for(int i=0;i<numPoints-1;i++) {
+			VPoint *point1 = vSticks[i]->getPointA();
+			VPoint *point2 = vSticks[i]->getPointB();
+			CCPoint point1_ = ccp(point1->x,point1->y);
+			CCPoint point2_ = ccp(point2->x,point2->y);
+			float stickAngle = ccpToAngle(ccpSub(point1_,point2_));
+			CCSprite *tmpSprite = ropeSprites[i];
+			tmpSprite->setPosition(ccpMidpoint(point1_,point2_));
+			tmpSprite->setRotation(-CC_RADIANS_TO_DEGREES(stickAngle));
+		}
+	}
+}
+
+void VRope::removeSprites()
+{
+    for(int i=0;i<numPoints-1;i++) {
+		CCSprite *tmpSprite = ropeSprites[i];
+        spriteSheet->removeChild(tmpSprite, true);
+	}
+    ropeSprites.clear();
+}
---
+++ Classes/vRope/vrope.h
@@ -0,0 +1,68 @@
+/*
+ 
+ MIT License.
+
+ Copyright (c) 2012 Creeng Ltd.
+ Copyright (c) 2012 Flightless Ltd.  
+ Copyright (c) 2010 Clever Hamster Games.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+*/
+
+//
+//  vrope.cpp
+//
+//  Created by patrick on 14/10/2010.
+//  Modified by Flightless www.flightless.co.nz 20/4/2012
+//  Ported to cocos2d-x by Creeng www.creeng.com 12/7/2012
+//
+
+#ifndef VROPE_H
+#define VROPE_H
+
+#include "vpoint.h"
+#include "vstick.h"
+#include "cocos2d.h"
+#include "Box2D/Box2D.h"
+
+#include <vector>
+
+//PTM_RATIO defined here is for testing purposes, it should obviously be the same as your box2d world or, better yet, import a common header where PTM_RATIO is defined
+#define PTM_RATIO 100
+
+class VRope
+{
+public:
+    VRope(b2RopeJoint *joint,  cocos2d::CCSpriteBatchNode* ropeBatchNode);
+    virtual ~VRope();
+
+    void update(float dt);
+    void reset();
+    
+    void createRope(const cocos2d::CCPoint& pointA, const cocos2d::CCPoint& pointB, float distance);
+    void resetWithPoints(const cocos2d::CCPoint &pointA, const cocos2d::CCPoint& pointB);
+    void updateWithPoints(const cocos2d::CCPoint& pointA, const cocos2d::CCPoint& pointB, float dt);
+    void updateWithPoints(const cocos2d::CCPoint& pointA, const cocos2d::CCPoint& pointB, float gxdt, float gydt); // Flightless, update with support for pre-integrating the gravity each step (optimised for changing gravity)
+    void debugDraw();
+    void updateSprites();
+    void removeSprites();
+
+private:
+    int numPoints;
+	
+    std::vector<VPoint*> vPoints;
+    std::vector<VStick*> vSticks;
+    std::vector<cocos2d::CCSprite*> ropeSprites;
+	
+    cocos2d::CCSpriteBatchNode* spriteSheet;
+    float antiSagHack;
+    b2RopeJoint *joint;
+    b2Joint *jointAB;
+};
+
+#endif  // VROPE_H
